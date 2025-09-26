30cae5ec8b64f5ba9ae9be4c71f915b90855b229
6ruby1 <ruby@rubygem.dev>
2025-09-26 22:42:46 +1000

    feat(app): add App impl and main-loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
+use ratatui::crossterm::event::{Event, read};
+use ratatui::{DefaultTerminal, Frame};
+
 fn main() {
     let _guard = sentry::init((
         "https://62cf52ffca04346f14507c4a08db9b2c@o4510067097665536.ingest.de.sentry.io/4510081161756752",
@@ -9,6 +14,12 @@ fn main() {
             ..Default::default()
         },
     ));
+
+    let mut terminal = ratatui::init();
+    let res = App::default().run(&mut terminal);
+    ratatui::restore();
+}
+
 #[derive(Debug)]
 pub struct App {
     query: String,
@@ -21,5 +32,21 @@ impl Default for App {
             query: String::new(),
             running: true,
         }
+    }
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while self.running {
+            terminal.draw(|frame| self.draw(frame))?;
+            if matches!(read()?, Event::Key(_)) {
+                self.running = false
+            }
+        }
+        Ok(())
+    }
+
+    pub fn draw(&self, frame: &mut Frame) {
+        frame.render_widget("hello world", frame.area());
     }
 }
